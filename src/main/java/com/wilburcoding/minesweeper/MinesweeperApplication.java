453a754c8cdeeebf93f3b91bf06001fe0d5dec78
Wilbur W <wilburwei.2008@gmail.com>
2024-05-30 21:21:29 -0400

    Base styling format

--- src/main/java/com/wilburcoding/minesweeper/MinesweeperApplication.java
+++ src/main/java/com/wilburcoding/minesweeper/MinesweeperApplication.java
@@ -11,8 +11,8 @@ public class MinesweeperApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MinesweeperApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 700);
+        stage.setTitle("Minesweeper");
         stage.setScene(scene);
         stage.show();
     }
