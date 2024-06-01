5b0cc2adcf5fae329c5e75d8d8bb5e45f7724a55
Wilbur W <wilburwei.2008@gmail.com>
2024-05-31 21:07:29 -0400

    Asynchronously update UI to create game UI

--- src/main/java/com/wilburcoding/minesweeper/MinesweeperApplication.java
+++ src/main/java/com/wilburcoding/minesweeper/MinesweeperApplication.java
@@ -1,11 +1,17 @@
 package com.wilburcoding.minesweeper;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +26,32 @@ public class MinesweeperApplication extends Application {
         ObservableList<String> difficultyList = FXCollections.observableArrayList(
                 "Easy", "Medium", "Hard");
         difficultyBox.setItems(difficultyList);
+        Button start = (Button) scene.lookup("#startButton");
+        EventHandler<ActionEvent> playHandler = e1 -> {
+            VBox mainGame = (VBox) scene.lookup("#mainGame");
+
+            Platform.runLater(() -> {
+                for (int i = 0; i < 20; i++) {
+                    HBox hbox = new HBox();
+                    hbox.prefHeight(25.0);
+                    hbox.prefWidth(500.0);
+                    for (int j = 0; j < 20; j++) {
+                        Button button = new Button();
+                        button.setText("1");
+                        button.prefHeight(25.0);
+                        button.prefWidth(25.0);
+                        button.setStyle("-fx-border-color: black");
+                        hbox.getChildren().add(button);
+
+
+                    }
+                    //Brutally learned that there has to be something inside the HBox for it to be shown
+                    mainGame.getChildren().add(hbox);
+                }
+            });
+        };
+        start.setOnAction(playHandler);
+
         stage.setTitle("Minesweeper");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/wilburcoding/minesweeper/MinesweeperController.java
+++ src/main/java/com/wilburcoding/minesweeper/MinesweeperController.java
@@ -1,27 +1,13 @@
 package com.wilburcoding.minesweeper;
 
-import javafx.fxml.FXML;
+//import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class MinesweeperController {
     public VBox mainGame;
-    @FXML
-    private Label mainText;
 
-    @FXML
-    protected void startButtonClick() {
-        mainText.setText("Minesweeper!!");
-        for (int i = 0; i < 20; i++) {
-            HBox hbox = new HBox();
-            hbox.prefHeight(25.0);
-            hbox.prefWidth(500.0);
-            hbox.setStyle("-fx-border-color: black");
-            hbox.getChildren().add(new Label(""));
-            //Brutally learned that there has to be something inside the HBox for it to be shown
-            mainGame.getChildren().add(hbox);
-        }
-
-    }
+    public Label mainText;
+    public Button startButton;
 }
