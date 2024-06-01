ddf78ee9be218bfe65a55f5d5430887a81aa9bed
Wilbur W <wilburwei.2008@gmail.com>
2024-05-31 21:51:45 -0400

    More game functionality + starting initial board generation method

--- src/main/java/com/wilburcoding/minesweeper/MinesweeperGame.java
+++ src/main/java/com/wilburcoding/minesweeper/MinesweeperGame.java
@@ -3,6 +3,21 @@ package com.wilburcoding.minesweeper;
 public class MinesweeperGame {
     private MinesweeperCell[][] board;
     public MinesweeperGame() {
-        board = new MinesweeperCell[9][9];
+        board = new MinesweeperCell[20][20];
+    }
+
+    public MinesweeperCell[][] getBoard() {
+        return board;
+    }
+
+    public void generateInitialBoard() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+
+        }
     }
 }
